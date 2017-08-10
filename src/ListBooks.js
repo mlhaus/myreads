449d78806bb464e09a1827e914362765c8a58421
Marc Hauschildt <marc.hauschildt@gmail.com>
2017-08-10 11:21:59 -0500

    Created SearchBooks and ListBooks components

---
+++ src/ListBooks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ListBooks extends Component {
+  render() {
+    return (
+      <div>ListBooks</div>
+    )
+  }
+}
+
+export default ListBooks
---
+++ src/SearchBooks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class SearchBooks extends Component {
+  render() {
+    return (
+      <div>SearchBooks</div>
+    )
+  }
+}
+
+export default SearchBooks
