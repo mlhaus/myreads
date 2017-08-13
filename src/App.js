27828fd57dc22db080ef18cdbbe423750fad8f4a
Marc Hauschildt <marc.hauschildt@gmail.com>
2017-08-13 15:35:10 -0500

    Added State to ListBooks Component

--- src/App.js
+++ src/App.js
@@ -5,24 +5,22 @@ import SearchBooks from './SearchBooks'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-const books = [
-  {
-    "id": 1,
-    "coverURL": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-    "title": "To Kill a Mockingbird",
-    "authors": "Harper Lee"
-  },
-  {
-    "id": 2,
-    "coverURL": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-    "title": "Ender's Game",
-    "authors": "Orson Scott Card"
-  }
-]
-
 class BooksApp extends React.Component {
   state = {
-
+    books: [
+      {
+        "id": 1,
+        "coverURL": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+        "title": "To Kill a Mockingbird",
+        "authors": "Harper Lee"
+      },
+      {
+        "id": 2,
+        "coverURL": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
+        "title": "Ender's Game",
+        "authors": "Orson Scott Card"
+      }
+    ]
   }
 
   render() {
@@ -30,7 +28,7 @@ class BooksApp extends React.Component {
       <div className="app">
         <Route exact path="/" render={() => (
           <ListBooks
-            books={books}
+            books={this.state.books}
           />
         )}/>
         <Route path="/search" component={SearchBooks}/>
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,37 +1,35 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-class ListBooks extends Component {
-  render() {
-    return (
-      <ol className="books-grid">
-        {this.props.books.map((book) => (
-          <li key={book.id} className="bookshelf-list-item">
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{
-                  width: 128,
-                  height: 193,
-                  backgroundImage: `url(${book.coverURL})`
-                }}></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
+function ListBooks (props) {
+  return (
+    <ol className="books-grid">
+      {props.books.map((book) => (
+        <li key={book.id} className="bookshelf-list-item">
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${book.coverURL})`
+              }}></div>
+              <div className="book-shelf-changer">
+                <select>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
               </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors}</div>
             </div>
-          </li>
-        ))}
-      </ol>
-    )
-  }
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors}</div>
+          </div>
+        </li>
+      ))}
+    </ol>
+  )
 }
 
 export default ListBooks
