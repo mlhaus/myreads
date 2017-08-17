894872860836c774a0266dbfb9c42a6cc8811024
Marc Hauschildt <marc.hauschildt@gmail.com>
2017-08-16 22:59:37 -0500

    Created BookShelf and modified ListBooks to organize App code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
-import ListBooks from './ListBooks'
+import { Route } from 'react-router-dom'
+import BookShelf from './BookShelf'
 import SearchBooks from './SearchBooks'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
@@ -37,51 +37,10 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ListBooks
-                      onChangeShelf={this.changeShelf}
-                      books={this.state.books.filter(book => book.shelf === "currentlyReading")}
-                    />
-                  </div>
-                </div>
-
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ListBooks
-                      onChangeShelf={this.changeShelf}
-                      books={this.state.books.filter(book => book.shelf === "wantToRead")}
-                    />
-                  </div>
-                </div>
-
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ListBooks
-                      onChangeShelf={this.changeShelf}
-                      books={this.state.books.filter(book => book.shelf === "read")}
-                    />
-                  </div>
-                </div>
-
-              </div>
-            </div>
-            <div className="open-search">
-              <Link
-                to="/search"
-                className="search-books"
-              >Add a book</Link>
-            </div>
-          </div>
+          <BookShelf
+            books = { this.state.books }
+            onChangeShelf = { this.changeShelf }
+          />
         )}/>
         <Route path="/search" component={SearchBooks}/>
       </div>
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,32 +3,37 @@ import PropTypes from 'prop-types'
 
 function ListBooks (props) {
   return (
-    <ol className="books-grid">
-      {props.books.map((book) => (
-        <li key={book.id} className="bookshelf-list-item">
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{
-                width: 128,
-                height: 193,
-                backgroundImage: `url(${book.imageLinks.thumbnail})`
-              }}></div>
-              <div className="book-shelf-changer">
-                <select className="book-change" onChange={(event) => props.onChangeShelf(book, event.target.value)}>
-                  <option value="none" disabled>Move to...</option>
-                  <option selected={book.shelf === "currentlyReading"} value="currentlyReading">Currently Reading</option>
-                  <option selected={book.shelf === "wantToRead"} value="wantToRead">Want to Read</option>
-                  <option selected={book.shelf === "read"} value="read">Read</option>
-                  <option selected={book.shelf === "none" || !book.shelf } value="none">None</option>
-                </select>
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{ props.shelfName }</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.map((book) => (
+            <li key={book.id} className="bookshelf-list-item">
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{
+                    width: 128,
+                    height: 193,
+                    backgroundImage: `url(${book.imageLinks.thumbnail})`
+                  }}></div>
+                  <div className="book-shelf-changer">
+                    <select className="book-change" onChange={(event) => props.onChangeShelf(book, event.target.value)}>
+                      <option value="none" disabled>Move to...</option>
+                      <option selected={book.shelf === "currentlyReading"} value="currentlyReading">Currently Reading</option>
+                      <option selected={book.shelf === "wantToRead"} value="wantToRead">Want to Read</option>
+                      <option selected={book.shelf === "read"} value="read">Read</option>
+                      <option selected={book.shelf === "none" || !book.shelf } value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
               </div>
-            </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors}</div>
-          </div>
-        </li>
-      ))}
-    </ol>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
   )
 }
 
